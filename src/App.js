7f775744fe13ccb6ba962d83e16bd9956d6f20a0
Rose <rose.yao@gmail.com>
2025-07-15 14:07:08 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -195,7 +195,7 @@ const library = {
                     title: "The Laundry Day Disaster",
                     image: "/Images/book3/0-intro.png",
                      text: "The village\'s magical laundry dryer has started eating everyone\'s clothes! Socks, shirts, and even Ivy\'s favorite purple ribbon have vanished. But when Ivy and Oliver investigate, they discover the dryer isn\'t mean - it\'s just VERY hungry and lonely. Can they find a way to help their new appliance friend while saving everyone\'s wardrobe?",
-                     { text: "Start Adventure", nextScene: 'scene1' },
+                     { text: "Start Adventure", nextScene: 'scene1' }
                     ]
                 },
                 scene1: {
